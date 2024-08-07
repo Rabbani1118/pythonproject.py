dfad9abb8629402e25cadc3297858bd8ce11ceaf
Rabbani1118 <khajarabbani654@gmail.com>
2024-08-07 18:23:12 +0530

    Add files via upload

---
+++ mypythonproject.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class QuizApp:
+    def __init__(self):
+        self.quiz_data = [  
+           {
+            "question":"Who developed Python Programming Language?",
+            "options":[" Wick van Rossum", " Rasmus Lerdorf", "Guido van Rossum", " Niene Stom"],
+            "correct_answer": 2
+           },
+           {
+            "question":"Who developed Python Programming Language?",
+            "options":[" Wick van Rossum", " Rasmus Lerdorf", "Guido van Rossum", " Niene Stom"],
+            "correct_answer": 2
+           },
+           {
+            "question":"Who developed Python Programming Language?",
+            "options":[" Wick van Rossum", " Rasmus Lerdorf", "Guido van Rossum", " Niene Stom"],
+            "correct_answer": 2
+           },
+           {
+            "question":"Who developed Python Programming Language?",
+            "options":[" Wick van Rossum", " Rasmus Lerdorf", "Guido van Rossum", " Niene Stom"],
+            "correct_answer": 2
+           },
+           {
+            "question":"Who developed Python Programming Language?",
+            "options":[" Wick van Rossum", " Rasmus Lerdorf", "Guido van Rossum", " Niene Stom"],
+            "correct_answer": 2
+           }
+        ]
+        self.current_question_index = 0
+        self.score = 0
+
+        self.window = tk.Tk()
+        self.window.title("Quiz App")
+
+        self.question_label = tk.Label(self.window, text="")
+        self.question_label.pack()
+
+        self.question_frame = tk.Frame(self.window)
+        self.question_frame.pack()
+
+        self.options_frame = tk.Frame(self.window)
+        self.options_frame.pack()
+
+        self.option_buttons =[]
+        for i in range(4):
+            button = tk.Button(self.options_frame, text="", width=30, command=lambda i=i: self.check_answer(i))
+            button.pack(pady=5)
+            self.option_buttons.append(button) 
+        self.next_question_button = tk.Button(self.window, text="Next Question", width=30, command=self.next_question)
+        self.next_question_button.pack(pady=10)
+    def check_answer(self, selected_option):
+        question_data = self.quiz_data[self.current_question_index]
+        answer =question_data["correct_answer"]
+        if selected_option == answer:
+            self.score = self.score + 1
+            messagebox.showinfo("correct", "your answer is correct")
+        else:
+             messagebox.showinfo("Incorrect", "your answer is wrong")
+    def load_question(self):
+        question_data = self.quiz_data[self.current_question_index]
+        self.question_label.config(text = question_data["question"])
+        options = question_data["options"]
+        for i in range(4):
+            self.option_buttons[i].config(text = options[i])
+    def next_question(self):
+        self.current_question_index += 1
+        if self.current_question_index == len(self.quiz_data):
+            messagebox.showinfo("Quiz over","your score is"+str(self.score))
+            self.window.quit()
+        else:
+            self.load_question()
+    def start_quiz(self):
+        self.load_question()
+        self.window.mainloop()
+quiz_app = QuizApp()
+quiz_app.start_quiz()
